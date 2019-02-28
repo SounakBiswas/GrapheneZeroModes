812077d7eb536c1360a44ea0e331bac27719e26e
Sounak Biswas <sounak.pablo@gmail.com>
2019-02-28 21:09:37 +0530

    armchair boundaries added

---
+++ initialize_semiopen.c
@@ -0,0 +1,219 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "global.h"
+#include "time.h"
+#include <stdlib.h>
+void initialize(){
+  lx=LX;
+  ly=LY;
+  ncells=NCELLS;
+  nlinks=NLINKS;
+  nedges=NEDGES;
+  nsites=NSITES;
+  nc=NC;
+  n_realizations=NR;
+  num_vacs=nc*ncells;
+  n_nonzeros=3*(ncells)-ly -6*num_vacs;
+  srand(SEED);
+  int i,j;
+}
+void allocate_arrays(){
+  cdegree=(int*)malloc(ncells*sizeof(int));
+  rdegree=(int*)malloc(ncells*sizeof(int));
+  rids=(int *)malloc(n_nonzeros*sizeof(int));
+  cptrs=(int *)malloc((ncells+1)*sizeof(int));
+  rmatch=(int*)malloc(ncells*sizeof(int));
+  cmatch=(int*)malloc(ncells*sizeof(int));
+}
+void free_arrays(){
+  free(cmatch);
+  free(rmatch);
+  free(rids);
+  free(cptrs);
+  free(cdegree);
+  free(rdegree);
+}
+
+void make_sparse(){
+  int i,j;
+  int x,y;
+  int cell1,cell2;
+  for(i=0;i<NSITES;i++)
+    ifvac[i]=0;
+  int sitea,siteb;
+
+  for(i=0;i<ncells;i++){
+    cdegree[i]=3;
+    rdegree[i]=3;
+  }
+  for (y=0;y<ly;y++){
+    x=y/2+y%2;
+    cell1=y*lx+x;
+    x=(x-1+lx)%lx;
+    cell2=y*lx+x;
+    
+    cdegree[cell1]=cdegree[cell2]=2;
+    rdegree[cell1]=rdegree[cell2]=2;
+    ifvac[2*cell1]=ifvac[2*cell1+1]=-1;
+    ifvac[2*cell2]=ifvac[2*cell2+1]=-1;
+  }
+
+  for(i=0;i<num_vacs;i++){
+    sitea=(int)((rand()/(RAND_MAX+1.0))*ncells)*2;
+    if(ifvac[sitea]==0){
+      x=(sitea/2)%lx;
+      y=(sitea/2)/lx;
+      ifvac[sitea]=1;
+      cdegree[sitea/2]=0;
+
+      siteb=neigh[sitea][0];
+      rdegree[siteb/2]--;
+      ifvac[siteb]=-1;
+      ifvac[neigh[siteb][1]]=-1;
+      ifvac[neigh[siteb][2]]=-1;
+
+      siteb=neigh[sitea][1];
+      rdegree[siteb/2]--;
+      ifvac[siteb]=-1;
+      ifvac[neigh[siteb][0]]=-1;
+      ifvac[neigh[siteb][2]]=-1;
+
+      siteb=neigh[sitea][2];
+      rdegree[siteb/2]--;
+      ifvac[siteb]=-1;
+      ifvac[neigh[siteb][1]]=-1;
+      ifvac[neigh[siteb][0]]=-1;
+    }
+    else
+      i--;
+  }
+  for(i=0;i<num_vacs;i++){
+    siteb=(int)((rand()/(RAND_MAX+1.0))*ncells)*2+1;
+    if(ifvac[siteb]==0){
+      //printf("%d\n",siteb);
+      x=(siteb/2)%lx;
+      y=(siteb/2)/lx;
+      ifvac[siteb]=1;
+      rdegree[siteb/2]=0;
+
+      sitea=neigh[siteb][0];
+      cdegree[sitea/2]--;
+      ifvac[sitea]=-1;
+      ifvac[neigh[sitea][1]]=-1;
+      ifvac[neigh[sitea][2]]=-1;
+
+      sitea=neigh[siteb][1];
+      cdegree[sitea/2]--;
+      ifvac[sitea]=-1;
+      ifvac[neigh[sitea][0]]=-1;
+      ifvac[neigh[sitea][2]]=-1;
+
+      sitea=neigh[siteb][2];
+      cdegree[sitea/2]--;
+      ifvac[sitea]=-1;
+      ifvac[neigh[sitea][1]]=-1;
+      ifvac[neigh[sitea][0]]=-1;
+    }
+    else
+      i--;
+  }
+
+  for(i=0;i<ncells;i++){
+    cmatch[i]=rmatch[i]=-1;
+    if(ifvac[2*i]==1)
+      cmatch[i]=-2;   // -2 : impossible to match.
+    if(ifvac[2*i+1]==1)
+      rmatch[i]=-2;
+  }
+
+  int rid_ctr,c_ctr;
+  rid_ctr=c_ctr=0;
+  int flagn1,flagn2;
+  for(i=0;i<ncells;i++){
+    cptrs[c_ctr]=rid_ctr;
+    c_ctr++;
+    x=(i)%lx;
+    y=(i)/lx;
+    flagn1=(y%2==1)&&(x== ((lx-1+y/2+y%2)%lx));
+    flagn2=(y%2==0)&&(x==(y/2+y%2));
+    if(ifvac[2*i]!=1){
+      siteb=neigh[2*i][0];
+      if(ifvac[siteb]!=1){
+        rids[rid_ctr]=siteb/2;
+        rid_ctr++;
+      }
+      if(!flagn1){
+      siteb=neigh[2*i][1];
+      if(ifvac[siteb]!=1){
+        rids[rid_ctr]=siteb/2;
+        rid_ctr++;
+      }
+      }
+      if(!flagn2){
+        siteb=neigh[2*i][2];
+        if(ifvac[siteb]!=1){
+          rids[rid_ctr]=siteb/2;
+          rid_ctr++;
+        }
+      }
+
+    }
+
+  }
+  printf("nz %d  tot %d\n",n_nonzeros,rid_ctr);
+  cptrs[ncells]=n_nonzeros;
+  printf("initialized\n");
+
+}
+void transpose(){
+  int i;
+  int rid_ctr,c_ctr;
+  int x,y;
+  int j,sitea,siteb;
+  int flagn1,flagn2;
+  n_nonzeros=3*ncells -ly -6*num_vacs;
+  int temp;
+  for(i=0;i<ncells;i++){
+    //cmatch[i]=rmatch[i]=-1;
+    //if(ifvac[2*i+1]==1)
+    //  cmatch[i]=-2;   // -2 : impossible to match.
+    //else if(ifvac[2*i]==1)
+    //  rmatch[i]=-2;
+
+    temp=cmatch[i];
+    cmatch[i]=rmatch[i];
+    rmatch[i]=temp;
+  }
+  rid_ctr=c_ctr=0;
+  for(i=0;i<ncells;i++){
+    cptrs[c_ctr]=rid_ctr;
+    c_ctr++;
+    if(ifvac[2*i+1]!=1){
+      x=(i)%lx;
+      y=(i)/lx;
+      sitea=neigh[2*i+1][0];
+    flagn2=(y%2==1)&&(x== ((lx-1+y/2+y%2)%lx));
+    flagn1=(y%2==0)&&(x==(y/2+y%2));
+      if(ifvac[sitea]!=1){
+        rids[rid_ctr]=sitea/2;
+        rid_ctr++;
+      }
+      if(!flagn1){
+      sitea=neigh[2*i+1][1];
+      if(ifvac[sitea]!=1){
+        rids[rid_ctr]=sitea/2;
+        rid_ctr++;
+      }
+      }
+
+      if( !flagn2){
+        sitea=neigh[2*i+1][2];
+        if(ifvac[sitea]!=1){
+          rids[rid_ctr]=sitea/2;
+          rid_ctr++;
+        }
+      }
+    }
+  }
+  cptrs[ncells]=n_nonzeros;
+}
---
+++ take_stats_semiopen.c
@@ -0,0 +1,184 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "global.h"
+#include "assert.h"
+#include "math.h"
+int take_stats(){
+  int pocket[NSITES];
+  int i,site,site2,sublat,cell,x,y;
+  int il;
+  int flagn1,flagn2;
+  char fname[200];
+  sprintf(fname,"./outfiles/cstats_Lx%dnc%.3fSEED%d.dat",LX,NC,NUM);
+  FILE *statf=fopen(fname,"a");
+  
+  il=0;
+  for (il=0;il<nsites;il++){
+    burn[il]=-1;
+  }
+
+  int count_free,count_ra,count_rb;
+  count_free=count_ra=count_rb=0;
+  int n_clust=0;
+  int ctype=0;
+  int pck_counter,clust_counter,boundary_counter;
+  int bflag;
+  int testcsize=0;
+  int sumi=0;
+  int num_clusts[3]={0,0,0};
+  int slat_imbalance;
+
+  for(i=0;i<nsites;i++){
+    if(   (ifvac[i]!=1) && (burn[i]==-1)){
+      pck_counter=clust_counter=-1;
+      boundary_counter=-1;
+      if(ifvac[i]<=0)
+        ctype=0;
+      else if(ifvac[i]==2){
+        if(i%2)
+          ctype=2;
+        else
+          ctype=1;
+      }
+      else if(ifvac[i]==3){
+        if(i%2)
+          ctype=1;
+        else
+          ctype=2;
+      }
+      num_clusts[ctype]+=1;
+      pck_counter++;
+      pocket[pck_counter]=i;
+      clust_counter++;
+      burn[i]=n_clust;
+      int i1;
+      slat_imbalance=0;
+      while(pck_counter!=-1){
+        site=pocket[pck_counter];
+        pck_counter--;
+        sublat=site%2;
+        cell=site/2;
+        x=cell%lx;
+        y=cell/lx;
+        bflag=0;
+        flagn1= ((sublat==1) && (y%2==0))&&(x==(y/2+y%2)) || ((sublat==0)&&(y%2==1)&&(x== ((lx-1+y/2+y%2)%lx)));
+        flagn2= ((sublat==0) && (y%2==0)&&(x==(y/2+y%2))) || ((sublat==1)&&(y%2==1)&&(x== ((lx-1+y/2+y%2)%lx)));
+        slat_imbalance+= (2*sublat-1);
+        //getchar();
+        if(ifvac[site]<=0){  //free clusters
+          site2=neigh[site][0];
+          if( (ifvac[site2]<=0) &&(burn[site2]==-1)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][1];
+          if((ifvac[site2]<=0) &&(burn[site2]==-1) && (!flagn1)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][2];
+          if( (ifvac[site2]<=0) &&(burn[site2]==-1) && (!flagn2)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+        }
+
+        if(ifvac[site]==2){  //R-motifs
+          site2=neigh[site][0];
+          if( (ifvac[site2]==3) &&(burn[site2]==-1)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][1];
+          if((ifvac[site2]==3) &&(burn[site2]==-1)&& (!flagn1)){
+            pck_counter++;
+            //printf("padd\n");
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][2];
+          if( (ifvac[site2]==3) &&(burn[site2]==-1)&& (!flagn2)){
+            pck_counter++;
+            //printf("padd\n");
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+        }
+
+        if(ifvac[site]==3){  //R-motifs
+          site2=neigh[site][0];
+          if( (ifvac[site2]==2) &&(burn[site2]==-1)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][1];
+          if((ifvac[site2]==2) &&(burn[site2]==-1)&& (!flagn1)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+          site2=neigh[site][2];
+          if( (ifvac[site2]==2) &&(burn[site2]==-1)&& (!flagn2)){
+            pck_counter++;
+            pocket[pck_counter]=site2;
+            clust_counter++;
+            burn[site2]=n_clust;
+          }
+          else 
+            bflag= bflag||(ifvac[site2]!=1)&&(burn[site2]!=n_clust);
+
+        }
+        boundary_counter+=bflag;
+
+      }
+      testcsize+= (clust_counter+1);
+      if(ctype!=0)
+      fprintf(statf,"%d %d %d %d %d\n",n_clust,ctype,clust_counter+1,boundary_counter+1,slat_imbalance);
+      n_clust++;
+      sumi+=abs(slat_imbalance);
+
+    }
+  }
+  printf("cluster counts :total %d,free %d, RA=%d, RB=%d\n",n_clust,num_clusts[0],num_clusts[1],num_clusts[2]);
+  printf("total sites %d %d lx=%d simb %d\n ",2*(ncells-num_vacs),testcsize,lx,sumi);
+  assert(2*(ncells-num_vacs)==testcsize);
+  fclose(statf);
+  return n_clust;
+}
